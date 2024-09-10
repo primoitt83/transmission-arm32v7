c2e7fac0849129d022c985d9df7ad7a38454ec67
primoitt83 <primoitt83@yahoo.com.br>
2024-09-10 17:21:47 -0300

    fix persmissions

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # change permissions for possible new files
-chown $APP_USERNAME.$APP_GROUPNAME -R /var/lib/transmission/downloads
-chown $APP_USERNAME.$APP_GROUPNAME -R /var/lib/transmission/watch
-chmod 775 -R /var/lib/transmission/downloads
-chown $APP_USERNAME.$APP_GROUPNAME -R /var/lib/transmission/watch
+chown $APP_USERNAME.$APP_GROUPNAME -R /var/lib/transmission
+chmod 775 -R /var/lib/transmission
 
 transmission-daemon --foreground --config-dir /var/lib/transmission/config
